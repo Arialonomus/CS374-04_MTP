93d139300085769b69194effc38dd02856db1d97
Jacob Barber <barber.jacobervin@gmail.com>
2024-02-18 18:59:59 -0600

    Added processing code files to hold related functions

---
+++ headers/processing.h
@@ -0,0 +1,10 @@
+/* Program: CS 374 Assignment 04 - MTP
+ *
+ * This file contains functions related to reading and processing a line of input
+ */
+#ifndef MTP_PROCESSING_H
+#define MTP_PROCESSING_H
+
+
+
+#endif //MTP_PROCESSING_H
