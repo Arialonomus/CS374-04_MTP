23895277748a9b6131ed8d56337285d8f9ac6c71
Jacob Barber <barber.jacobervin@gmail.com>
2024-02-25 14:35:32 -0600

    Added check_stop() function to determine if the stop sequence is the next line

--- headers/processing.h
+++ headers/processing.h
@@ -2,9 +2,37 @@
  *
  * This file contains functions related to reading and processing a line of input
  */
+
 #ifndef MTP_PROCESSING_H
 #define MTP_PROCESSING_H
 
+#include <err.h>
+#include <string.h>
+#include <stdbool.h>
+#include "buffers.h"
 
+enum status_t
+{
+ INPROGRESS,
+ STOPPED,
+ FINISHED,
+ ERROR
+};
+
+// Reads characters from stdin and stores them in input_buf
+// Terminates input if the stop processing sequence "STOP\n" is received
+void* get_input(void* arg);
+
+// Returns true if the next line of input is "STOP\n"
+bool check_stop();
+
+// Converts all line separator characters from input into spaces
+void* ls_converter(void* arg);
+
+// Converts all instances of "++" to "^"
+void* plus_converter(void* arg);
+
+// Prints processed output to stdout in lines of 80 characters
+void* print_output(void* arg);
 
 #endif //MTP_PROCESSING_H
