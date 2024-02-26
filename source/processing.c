18fb5f91c94c449c5d85995027ec485b87c5f627
Jacob Barber <barber.jacobervin@gmail.com>
2024-02-25 18:18:05 -0600

    Refactored get_input() to use new buffer maintenance functions

--- source/processing.c
+++ source/processing.c
@@ -4,13 +4,12 @@ void* get_input(void* arg)
 {
 	// Initialize iterator
 	struct sharedbuffer* shr_buf = &shared[INPUT];
-	enum status_t status = INPROGRESS;
 	size_t current = 0;
 	size_t end = BUF_SIZE - 1;
-	int setw_status = -1;
 
-	while(status == INPROGRESS) {
-		while(current != end && !feof(stdin)) {
+	enum status_t status = INPROGRESS;
+	for(;;) {
+		while(status == INPROGRESS && current != end) {
 			// Read a char from input and write it to the buffer
 			int c = getchar();
 			if (ferror(stdin)) err (1, "getchar");
@@ -22,27 +21,23 @@ void* get_input(void* arg)
 			if (++current == BUF_SIZE) current = 0;
 
 			// Update the write barrier if not locked
-			setw_status = set_wbarrier(shr_buf, current);
+			set_barrier_pos(shr_buf, WRITE, current, CONTINUE);
 
 			// Check for STOP sequence after newline
 			if (c == '\n' && check_stop() == true) {
 				c = 4;
 				shr_buf->buffer[current] = (char)c;
 				status = STOPPED;
-				while (setw_status != 0)
-					setw_status = set_wbarrier(shr_buf, current);
-				return (void*)status;
 			}
 		}
-		// Get new end position if not locked
-		get_rbarrier(shr_buf, &end);
-	}
+		// Update the write barrier for downstream thread
+		set_barrier_pos(shr_buf, WRITE, current, WAIT);
 
-	// Set status and return
-	status = FINISHED;
-	while (setw_status != 0)
-		setw_status = set_wbarrier(shr_buf, current);
-	return (void*)status;
+		// Wait for downstream thread to process more data
+		if (current == end && status == INPROGRESS)
+			hold(shr_buf, READ, current, &end);
+		else return (void*)status;
+	}
 }
 
 bool check_stop()
@@ -114,7 +109,7 @@ void* ls_converter(void* arg)
 			if (in_current == in_end)
 				hold(input_buf, WRITE, in_current, &in_end);
 
-			// Wait for output stream to process more data
+			// Wait for downstream thread to process more data
 			if (out_current == out_end)
 				hold(output_buf, READ, out_current, &out_end);
 		}
