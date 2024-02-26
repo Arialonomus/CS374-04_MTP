a2825ade25e1b908bd2b455eedd018c79d3f7f93
Jacob Barber <barber.jacobervin@gmail.com>
2024-02-25 19:05:15 -0600

    Added code to create and join threads

--- source/line_processor.c
+++ source/line_processor.c
@@ -1,5 +1,5 @@
 /* Program: CS 374 Assignment 04 - MTP
- * Author: Jacob Barber
+* Author: Jacob Barber
  * UID: 934561945
  *
  * This program performs simple multi-threaded line processing to demonstrate understanding
@@ -17,35 +17,43 @@
 #include <err.h>
 #include <stdio.h>
 #include <pthread.h>
-#include <bits/pthreadtypes.h>
+#include <stdlib.h>
 
 #include "processing.h"
 
-#define MAX_LINE_LEN 80
-#define BUF_SIZE MAX_LINE_LEN + 2
-
 int main(int argc, char *argv[])
 {
- /* Open file for reading */
- if (argc != 2) errx(1, "program takes a single filepath argument");
- char* input_filepath = argv[1];
- if(!freopen(input_filepath, "re", stdin)) err(1, "fopen(): %s", input_filepath);
+	// Open file for reading
+	if (argc == 2) {
+		char* input_filepath = argv[1];
+		if(!freopen(input_filepath, "re", stdin)) err(1, "freopen(): %s", input_filepath);
+	}
+	else if (argc > 2) errx(1, "too many arguments");
 
- /* Initialize buffers & their associated mutexes */
- char input_buf[BUF_SIZE] = {0};
- size_t input_index = 0;
- pthread_mutex_t input_mutex = PTHREAD_MUTEX_INITIALIZER;
- pthread_cond_t input_sig = PTHREAD_COND_INITIALIZER;
+	// Initialize shared buffers
+	for (int i = 0; i < NUM_BUFS; ++i) {
+		pthread_mutex_init(&shared[i].mutex, NULL);
+		pthread_cond_init(&shared[i].condition, NULL);
+	}
 
- char process_buf[BUF_SIZE] = {0};
- size_t process_index = 0;
- pthread_mutex_t process_mutex = PTHREAD_MUTEX_INITIALIZER;
- pthread_cond_t process_sig = PTHREAD_COND_INITIALIZER;
+	// Create process threads
+	pthread_t input_reader;
+	pthread_create(&input_reader, NULL, get_input, NULL);
 
- char output_buf[BUF_SIZE] = " ";
- size_t output_index = 0;
- pthread_mutex_t outputm_mutex = PTHREAD_MUTEX_INITIALIZER;
- pthread_cond_t output_sig = PTHREAD_COND_INITIALIZER;
+	pthread_t newline_converter;
+	pthread_create(&newline_converter, NULL, convert_newline, NULL);
 
+	pthread_t plus_converter;
+	pthread_create(&plus_converter, NULL, convert_doubleplus, NULL);
 
+	pthread_t output_printer;
+	pthread_create(&output_printer, NULL, print_output, NULL);
+
+	// Join threads after processing is complete
+	if (pthread_join(input_reader, NULL) != 0) err(1, "pthread_join(input_reader)");
+	if (pthread_join(newline_converter, NULL) != 0) err(1, "pthread_join(input_reader)");
+	if (pthread_join(plus_converter, NULL) != 0) err(1, "pthread_join(input_reader)");
+	if (pthread_join(output_printer, NULL) != 0) err(1, "pthread_join(input_reader)");
+
+	return EXIT_SUCCESS;
 }
